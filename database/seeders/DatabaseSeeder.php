55e28fb2a5949dbcd3338afc7cd8fb15379297cb
Konstantinos Kazazis <kazaziskonstantinos@proton.me>
2026-07-12 11:24:16 +0200

    can seed admin credentials, admin role and user_roles for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;   
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Role::create(
+            [
+                'id' => 1,
+                'name' => 'admin',
+            ]
+        );
 
         User::factory()->create([
+            'id' => 1,
             'username' => 'Admin',
             'email' => 'admin@proton.me',
             'password' =>  password_hash(123456789, PASSWORD_BCRYPT)
         ]);
+
+        DB::table('user_roles')->insert(
+            [
+                'role_id' => 1,
+                'user_id' => 1
+            ]
+        );
+
     }
 }
