69631970e3f18aec8a1f0dd97a08cad367965d1b
Konstantinos Kazazis <kazaziskonstantinos@proton.me>
2026-07-28 19:55:31 +0200

    login redirects to dashboards

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
+class LoginResponse implements LoginResponseContract
+{
+    public function toResponse($request)
+    {
+        $user = $request->user();
+
+        $route = match (true) {
+            $user->role('admin') => 'admin.dashboard',
+            $user->role('user')  => 'user.dashboard',
+            default              => 'home',
+        };
+
+        return redirect()->intended(route($route));
+    }
+}
